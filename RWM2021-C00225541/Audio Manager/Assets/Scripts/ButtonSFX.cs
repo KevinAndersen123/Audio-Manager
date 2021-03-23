62c94688a593009489770d01341c34039a68f526
Kevin Andersen <c00225541@itcarlow.ie>
2021-03-23 13:31:57 +0000

    Added project to public repo

---
+++ RWM2021-C00225541/Audio Manager/Assets/Scripts/ButtonSFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSFX : MonoBehaviour
+{
+    public AudioSource mySfx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    //plays the hover sound effect
+    public void HoverSound()
+    {
+        mySfx.PlayOneShot(hoverFx);
+    }
+    //plays the click sound effect
+    public void ClickSound()
+    {
+        mySfx.PlayOneShot(clickFx);
+    }
+}
---
+++ RWM2021-C00225541/Audio Manager/Assets/Scripts/RotationScript.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    //rotates object
+    void Update()
+    {
+        transform.Rotate((Vector3.up * 50 * Time.deltaTime), Space.Self);
+    }
+}
